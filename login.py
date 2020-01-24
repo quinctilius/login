00a6957fa94a70b324e70e8310a540035b120a28
Ben Shillitoe <bshillitoe@gmail.com>
2020-01-24 03:45:41 +0000

    Beginning to set up password input

--- login.py
+++ login.py
@@ -4,19 +4,23 @@ print(welcome)
 
 def login():
     global username
+    global password
+    global password2
     for retry in range(5):
         welcome_input = input("login or create?")
         if welcome_input == "login":
             username = input("Please enter username")
+            password = input("Please enter your password")
             break
         if welcome_input == "create":
             username = input("Please choose a username")
+            password = input("Please choose a password")
+            password2 = input("Please confirm you chose password")
             break
         print("Sorry, please type login or create")
     else:
         print("Sorry, you don't get this")
         username = ""
-        
 
 login()
 
@@ -24,3 +28,6 @@ if username != "":
     print(username)
 elif username == "":
     print("No username available")
+
+if password != "":
+    print(password)
