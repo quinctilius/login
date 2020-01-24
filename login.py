465e03890564cb419b17bc8f04561ca62d39d45d
Ben Shillitoe <bshillitoe@gmail.com>
2020-01-24 04:09:03 +0000

    Further work on password creation

--- login.py
+++ login.py
@@ -6,7 +6,7 @@ def login():
     global username
     global password
     global password2
-    for retry in range(5):
+    for retry in range(5): #5 attempts at typing login or create
         welcome_input = input("login or create?")
         if welcome_input == "login":
             username = input("Please enter username")
@@ -14,8 +14,14 @@ def login():
             break
         if welcome_input == "create":
             username = input("Please choose a username")
-            password = input("Please choose a password")
-            password2 = input("Please confirm you chose password")
+            for retry in range(2): #two attempts at creating a password
+                password = input("Please choose a password")
+                password2 = input("Please confirm you chose password")
+                if password == password2:
+                    break
+                if password != password2:
+                    print("Sorry your passwords don't match, try again")
+                    password = ""
             break
         print("Sorry, please type login or create")
     else:
@@ -25,9 +31,11 @@ def login():
 login()
 
 if username != "":
-    print(username)
+    print("Well done, your user name is " + username)
 elif username == "":
     print("No username available")
 
 if password != "":
     print(password)
+elif password == "":
+    print("Error in password input")
